690ddbc3031af256b1ffa7dbef3a95821e7abc60
Chisanan232 <chi10211201@cycu.org.tw>
2026-05-13 09:42:34 +0800

    ✨ (analytics): Add Wong colorblind-safe palette and extend useChartPalette

--- dashboard/src/features/analytics/useChartPalette.ts
+++ dashboard/src/features/analytics/useChartPalette.ts
@@ -1,7 +1,8 @@
-import { CHART_CATEGORICAL_PALETTE } from './chartPalette'
+import { CHART_CATEGORICAL_PALETTE, CHART_COLORBLIND_PALETTE } from './chartPalette'
 
 const PALETTES = {
   categorical: CHART_CATEGORICAL_PALETTE,
+  colorblind: CHART_COLORBLIND_PALETTE,
 }
 
 export function useChartPalette(type: keyof typeof PALETTES): string[] {
